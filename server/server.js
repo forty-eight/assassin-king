85cab9e16664a2ef8240a295a4a223d69a83d3b9
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-08-12 00:57:43 -0400

    Server impl

--- server/server.js
+++ server/server.js
@@ -1,15 +1,35 @@
 var express = require('express');
+var crypto = require('crypto');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var sockets = {};
+var socketIds = [];
+
 app.use(express.static('../client'));
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('position', function(pos){
-    console.log(pos);
+  var id = crypto.pseudoRandomBytes(5).toString('hex');
+  console.log('a user connected with id: %s', id);
+
+  socket.broadcast.emit('newPeer', id);
+  socket.emit('initialConnection', socketIds);
+
+  socket.on('offer', function(event){
+    sockets[event.id].emit('offer', {id: id, description: event.description});
   });
+
+  socket.on('answer', function(event){
+    sockets[event.id].emit('answer', {id: id, description: event.description});
+  });
+
+  socket.on('iceCandidate', function(event){
+    sockets[event.id].emit('iceCandidate', {id: id, candidate: event.candidate});
+  });
+
+  sockets[id] = socket;
+  socketIds.push(id);
 });
 
 
